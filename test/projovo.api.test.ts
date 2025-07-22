159e557158c306c848261bd81bcac7562bd4329a
Jalen Greene <terrell.greene95@gmail.com>
2025-07-21 20:42:38 -0500

    init cdk

---
+++ test/projovo.api.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as ProjovoApi from '../lib/projovo.api-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/projovo.api-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new ProjovoApi.ProjovoApiStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
